03ab7e9e540c3a449b9d3ffeeefd1edc9c54e1be
Kees Wiegel <developer@iwantwin.nl>
2020-12-20 15:43:06 +0100

    Create day1part1.ts

---
+++ day1part1.ts
@@ -0,0 +1,13 @@
+function findAnswerFromItems( expectedAnswer: number, expenses: Array<string> ): number {
+  for( let it = 0; it < expenses.length; it++ ) {
+    const expense = parseInt(expenses[it], 10);
+    const requiredAnswer = expectedAnswer - expense;
+    if( expenses.indexOf( requiredAnswer.toString(), it ) !== -1 ) {
+      return (expense * requiredAnswer);
+    }
+  }
+}
+
+const expectedAnswer = 2020;
+const expenses = document.getElementsByTagName('pre')[0].innerHTML.split('\n');
+console.log( findAnswerFromItems( expectedAnswer, expenses ) );
